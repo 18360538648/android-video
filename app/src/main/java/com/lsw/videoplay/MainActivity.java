9646bc12e6f4475055d9f7b80a43b47b6034e427
Luo Siwei <luosw@bie-plc.com>
2017-08-29 16:19:48 +0800

    使用jcenter库

--- app/src/main/java/com/lsw/videoplay/MainActivity.java
+++ app/src/main/java/com/lsw/videoplay/MainActivity.java
@@ -3,6 +3,7 @@ package com.lsw.videoplay;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.lsw.playvideo.media.PlayStateParams;
 import com.lsw.playvideo.media.PlayerView;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,6 +13,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        playerView = new PlayerView(MainActivity.this).setPlaySource("http://9890.vod.myqcloud.com/9890_4e292f9a3dd011e6b4078980237cc3d3.f20.mp4").startPlay();
+        playerView = new PlayerView(this)
+                .setProcessDurationOrientation(PlayStateParams.PROCESS_PORTRAIT)
+                .setScaleType(PlayStateParams.fillparent)
+                .forbidTouch(false)
+                .hideBack(true)
+                .hideMenu(true)
+                .hideSteam(true)
+                .hideFullscreen(true)
+                .hideCenterPlayer(false)
+                .setForbidDoulbeUp(true)
+                .clickFinish(true)
+                .setNetWorkTypeTie(false)
+                .setPlaySource("http://9890.vod.myqcloud.com/9890_4e292f9a3dd011e6b4078980237cc3d3.f20.mp4").startPlay();
     }
 }
